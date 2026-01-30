45fd7c1d1c3e26648fa2babea323aedaa64ab206
ThomasDequirez <thomas.dequirez@laposte.net>
2026-01-30 14:05:30 +0100

    Deplacement .h, suppression .o et autres

--- lib/headers/player.h
+++ lib/headers/player.h
@@ -2,7 +2,7 @@
 #define PLAYER_H
 
 #include "types.h"
-#include "../lib/linux/raylib-5.5_linux_amd64/include/raymath.h"
+#include "../linux/raylib-5.5_linux_amd64/include/raymath.h"
 
 // Réinitialise les variables du joueur (position spawn, vitesse nulle)
 void InitPlayer(Player *player);
