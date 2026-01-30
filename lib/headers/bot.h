4bc81e10874208e96ab097a506414584e92060f4
ThomasDequirez <thomas.dequirez@laposte.net>
2026-01-30 14:05:30 +0100

    Deplacement .h, suppression .o et autres

--- lib/headers/bot.h
+++ lib/headers/bot.h
@@ -2,7 +2,7 @@
 #define BOT_H
 
 #include "types.h"
-#include "../lib/linux/raylib-5.5_linux_amd64/include/raymath.h"
+#include "../linux/raylib-5.5_linux_amd64/include/raymath.h"
 
 void InitBot(Bot *bot); // Réinitialise les variables du bot (position spawn, vitesse nulle)
 void UpdateBot(Bot *bot, Block blocks[NUM_BLOCKS][NUM_BLOCKS], Camera3D *camera);// Modifie l'état du bot et met à jour sa 'camera'  + gère IA, physique et collisions
