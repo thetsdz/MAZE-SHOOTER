28addc7abd26a00f44090aef7a529ffa88c23691
arblakmin <cjammes50@gmail.com>
2026-02-05 11:43:16 +0100

    idem

---
+++ lib/headers/sauvegarde.h
@@ -0,0 +1,16 @@
+#ifndef SAUVEGARDE_H
+#define SAUVEGARDE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "types.h"
+
+/** @brief Sauvegarde les données du joueur dans un fichier texte et l'encrypte
+ * directement*/
+void sauvegarder(Entity* player, int* score);
+
+/** @brief Charge les données cryptés du joueur depuis un fichier texte*/
+void chargerSauvegarde(Entity* player, int* score);
+
+#endif
