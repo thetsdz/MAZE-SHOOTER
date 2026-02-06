9a793e7154655ae387c35b68f69a6438b0f8a9ea
ThomasDequirez <thomas.dequirez@laposte.net>
2026-02-06 13:35:47 +0100

    Commit

--- doc/html/dir_6c53b035f36830854f983e52e14ea439.js
+++ doc/html/dir_6c53b035f36830854f983e52e14ea439.js
@@ -4,8 +4,10 @@ var dir_6c53b035f36830854f983e52e14ea439 =
     [ "bot.h", "bot_8h_source.html", null ],
     [ "cryptage.h", "cryptage_8h_source.html", null ],
     [ "level.h", "level_8h_source.html", null ],
+    [ "menu.h", "menu_8h_source.html", null ],
     [ "pile.h", "pile_8h_source.html", null ],
     [ "player.h", "player_8h_source.html", null ],
     [ "projectile.h", "projectile_8h_source.html", null ],
+    [ "sauvegarde.h", "sauvegarde_8h_source.html", null ],
     [ "types.h", "types_8h_source.html", null ]
 ];
--- doc/html/search/all_9.js
+++ doc/html/search/all_9.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['main_2ec_0',['main.c',['../main_8c.html',1,'']]],
-  ['maxammo_1',['maxAmmo',['../structEntity.html#a26274b451ad5d4b0b9c4b6748e6809ef',1,'Entity']]]
+  ['maxammo_0',['maxAmmo',['../structEntity.html#a26274b451ad5d4b0b9c4b6748e6809ef',1,'Entity']]]
 ];
