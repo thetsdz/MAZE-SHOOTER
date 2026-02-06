29c3b69691b973ae83fd9bff86c8b1ef8e381cf4
ThomasDequirez <thomas.dequirez@laposte.net>
2026-02-06 08:14:34 +0100

    modifs

--- src/menu.c
+++ src/menu.c
@@ -1,28 +1,43 @@
 #include "../lib/linux/raylib-5.5_linux_amd64/include/raylib.h"
 #include "../lib/linux/raylib-5.5_linux_amd64/include/raymath.h"
 #include "../lib/headers/types.h"
- /*pour compiler :
-  gcc menu.c -o menu -I../lib/linux/raylib-5.5_linux_amd64/include -I../lib/headers ../lib/linux/raylib-5.5_linux_amd64/lib/libraylib.a -lGL -lm -lpthread -ldl -lrt -lX11 
-*/
+#include "../lib/headers/menu.h"
 
 
-#include "raylib.h"
-#include "types.h"
 
-#include "raylib.h"
-#include "types.h"
-
-// Fonction de bouton (inchangée mais cruciale pour la propreté)
 bool DessinerBouton(Rectangle rect, const char* texte) {
     Vector2 mousePos = GetMousePosition();
     bool survol = CheckCollisionPointRec(mousePos, rect);
-    
+
     DrawRectangleLinesEx(rect, 2, survol ? WHITE : GRAY);
     int textWidth = MeasureText(texte, 20);
     DrawText(texte, rect.x + (rect.width - textWidth)/2, rect.y + (rect.height - 20)/2, 20, survol ? WHITE : LIGHTGRAY);
 
     return (survol && IsMouseButtonPressed(MOUSE_LEFT_BUTTON));
 }
+
+void GererMenu(GameScreen* currentScreen) {
+    int sw = GetScreenWidth();
+    int sh = GetScreenHeight();
+
+    float btnW = 300;
+    float btnH = 50;
+    float posX = (sw - btnW) / 2.0f;
+    float departY = sh / 3.0f;
+
+    DrawText("MENU PRINCIPAL", sw/2 - MeasureText("MENU PRINCIPAL", 30)/2, departY - 80, 30, RAYWHITE);
+
+    if (DessinerBouton((Rectangle){posX, departY, btnW, btnH}, "LANCER PARTIE")) {
+        *currentScreen = TEST; // Transition vers l'état "TEST" (jeu)
+    }
+    if (DessinerBouton((Rectangle){posX, departY + 70, btnW, btnH}, "OPTIONS")) {
+        *currentScreen = OPTIONS; // Transition vers l'état "OPTIONS"
+    }
+    if (DessinerBouton((Rectangle){posX, departY + 140, btnW, btnH}, "QUITTER")) {
+        CloseWindow();
+    }
+}
+
 
 int main(void) {
     // Initialisation avec le flag RESIZABLE pour aider au redimensionnement
