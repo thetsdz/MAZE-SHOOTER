36affb2f4ee96f6d89b63ee8540e918398b8faec
Hugues <hugues.astier.etu@univ-lemans.fr>
2026-02-11 15:25:48 +0100

    Ajout de documentation Doxygen dans le menu.h menu.c option.h option.c

--- lib/headers/option.h
+++ lib/headers/option.h
@@ -1,1 +1,28 @@
-void GererOption(GameScreen *currentScreen);
+#ifndef MENU_H
+#define MENU_H
+
+#include "raylib.h"
+#include "types.h"
+
+
+
+/**
+*   \brief Gére la creation/dessin des chaques boutons et zone d'informations
+*   \param <GameScreen* currentScreen> Etat de l'ecran actuel
+ */
+void GererOption(GameScreen* currentScreen);
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
