ceee7414e495e6210de4408f33736acf53ebbf57
arblakmin <cjammes50@gmail.com>
2026-03-11 12:02:41 +0100

    Ajout d'un peu de mise en place pour la doc(notamment le versionnage)

--- lib/headers/multijoueur.h
+++ lib/headers/multijoueur.h
@@ -3,25 +3,61 @@
 
 #include "reseau.h"
 #include "types.h"
-
-// Initialise les positions de départ opposées
+/** \version 1.0
+ *  \author Corentin Jammes
+ *  \date 02/02/2026
+ *  \brief Toutes les fonctions du mode multijoueur (Lobby + Gameplay)
+ */
+/**
+ * \brief Initialise les positions de départ opposées
+ * \param <Entity* joueur> le joueur local(serveur)
+ * \param <Entity* ennemi> l'ennemi distant(client)
+ * \param <int estServeur> 1 si le joueur est le serveur, 0 sinon
+ */
 void InitMultijoueur(Entity* joueur, Entity* ennemi, int estServeur);
 
-// La boucle principale du mode multi
+/**
+ * \brief La boucle principale du mode multi
+ * \param <Entity* joueur> le joueur local(serveur)
+ * \param <Entity* ennemi> l'ennemi distant(client)
+ * \param <Block blocks[NUM_BLOCKS][NUM_BLOCKS]> le tableau des blocs
+ * \param <Projectile projs[MAX_PROJ]> le tableau des projectiles
+ * \param <Camera3D* camera> la caméra
+ * \param <ReseauState* reseau> l'état du réseau
+ */
 void UpdateMultijoueur(Entity* joueur, Entity* ennemi,
                        Block blocks[NUM_BLOCKS][NUM_BLOCKS],
                        Projectile projs[MAX_PROJ], Camera3D* camera,
                        ReseauState* reseau);
 
-// Fonction pour gérer la partie multijoueur (lobby + jeu)
+/**
+ * \brief Fonction pour gérer la partie multijoueur (lobby + jeu)
+ * \param <Entity* player> le joueur local
+ * \param <Entity* remotePlayer> le joueur distant
+ * \param <Block blocks[NUM_BLOCKS][NUM_BLOCKS]> le tableau des blocs
+ * \param <Projectile projs[MAX_PROJ]> le tableau des projectiles
+ * \param <Camera3D* camera> la caméra
+ * \param <ReseauState* netState> l'état du réseau
+ * \param <bool* jeuInitialise> un pointeur vers une variable indiquant si le jeu est initialisé
+ * \param <int* score> un pointeur vers le score du joueur
+ * \param <GameScreen* currentScreen> un pointeur vers l'écran de jeu actuel
+ */
 void partie_multijoueur(Entity* player, Entity* remotePlayer,
                         Block blocks[NUM_BLOCKS][NUM_BLOCKS],
                         Projectile projs[MAX_PROJ], Camera3D* camera,
                         ReseauState* netState, bool* jeuInitialise, int* score,
                         GameScreen* currentScreen);
 
+/**
+ * \brief Dessine le lobby d'attente multijoueur
+ * \param <ReseauState* netState> etat du reseau
+ */
 void DessinerLobbyMultijoueur(ReseauState* netState);
 
+/**
+ * \brief Dessine le mode multijoueur 
+ * \param <Entity* player> le joueur local
+ */
 void DessinerMultijoueur(Entity* player, Entity* remotePlayer,
                          Block blocks[NUM_BLOCKS][NUM_BLOCKS],
                          Projectile projs[MAX_PROJ], Camera3D* camera,
--- lib/headers/sauvegarde.h
+++ lib/headers/sauvegarde.h
@@ -1,16 +1,46 @@
 #ifndef SAUVEGARDE_H
 #define SAUVEGARDE_H
 
+/**
+ *  \version 1.0
+ *  \author Corentin Jammes
+ *  \date 15/01/2026
+ *  \brief Fichier de sauvegarde en clair dans un fichier txt
+ */
+
+/**
+ *  \version 2.0
+ *  \author Corentin Jammes
+ *  \date 05/02/2026
+ *  \brief Fichier de sauvegarde sécurisé (Checksum + RC4) pour eviter la triche
+
+/**
+ *  \version 2.1
+ *  \author Corentin Jammes
+ *  \date 02/03/2026
+ *  \brief Fichier de sauvegarde sécurisé (Checksum + RC4) pour eviter la triche
+et ajout du bot dans le fichier de sauvegarde
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "types.h"
 
 /** @brief Sauvegarde les données du joueur dans un fichier texte et l'encrypte
- * directement*/
+ * directement
+ * \param <Entity* player> récupère les infos du joueur
+ * \param <Entity* bot> récupère les infos du bot
+ * \param <int* score> récupère le score de la partie
+ */
 void sauvegarder(Entity* player, Entity* bot, int* score);
 
-/** @brief Charge les données cryptés du joueur depuis un fichier texte*/
+/** @brief Charge les données cryptés du joueur depuis un fichier texte
+ *   \param <Entity* player> charge les infos du joueur
+ *   \param <Entity* bot> charge les infos du bot
+ *   \param <int* score> charge le score de la partie
+ */
 void chargerSauvegarde(Entity* player, Entity* bot, int* score);
 
 #endif
